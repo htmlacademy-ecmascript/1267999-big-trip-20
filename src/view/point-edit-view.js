023a600e6580257dcb351545cca512a5766f03ad
Konstanin <lable_01@mail.ru>
2023-06-22 20:59:25 +0300

    refactoring

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -8,7 +8,14 @@ function createTypeItem(types, point) {
     const checked = (type === typePoint) ? 'checked' : '';
     return (`
       <div class="event__type-item">
-        <input id="event-type-${type.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type.toLowerCase()}" ${checked}>
+        <input
+          id="event-type-${type.toLowerCase()}-1"
+          class="event__type-input visually-hidden"
+          type="radio"
+          name="event-type"
+          value="${type.toLowerCase()}"
+          ${checked}
+        >
         <label class="event__type-label  event__type-label--${type.toLowerCase()}" for="event-type-${type.toLowerCase()}-1">${type}</label>
       </div>
   `);
@@ -92,18 +99,35 @@ function createPointEditTemplate({point, pointDestinations, pointOffers}) {
             <label class="event__label  event__type-output" for="event-destination-1">
               ${type}
             </label>
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" list="destination-list-1">
+            <input
+             class="event__input event__input--destination"
+             id="event-destination-1"
+             type="text"
+             name="event-destination"
+             list="destination-list-1"
+            >
             <datalist id="destination-list-1">
               ${createCityItems(pointDestinations)}
             </datalist>
           </div>
 
-          <div class="event__field-group  event__field-group--time">
+          <div class="event__field-group event__field-group--time">
             <label class="visually-hidden" for="event-start-time-1">From</label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${getCurrentDate(dateFrom)}">
+            <input
+              class="event__input event__input--time"
+              id="event-start-time-1"
+              type="text"
+              name="event-start-time"
+              value="${getCurrentDate(dateFrom)}"
+            >
             &mdash;
             <label class="visually-hidden" for="event-end-time-1">To</label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${getCurrentDate(dateTo)}">
+            <input
+              class="event__input event__input--time"
+              id="event-end-time-1"
+              type="text" name="event-end-time"
+              value="${getCurrentDate(dateTo)}"
+            >
           </div>
 
           <div class="event__field-group  event__field-group--price">
--- src/view/trip-filters-view.js
+++ src/view/trip-filters-view.js
@@ -4,7 +4,13 @@ import {formatStringToCapital} from '../utils/point.js';
 function createFilterItem(filter) {
   return (`
     <div class="trip-filters__filter">
-      <input id="filter-${filter.type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.type}" ${(filter.hasPoints) ? '' : 'disabled'}>
+      <input
+        id="filter-${filter.type}"
+        class="trip-filters__filter-input visually-hidden"
+        type="radio"
+        name="trip-filter"
+        value="${filter.type}" ${(filter.hasPoints) ? '' : 'disabled'}
+      >
       <label class="trip-filters__filter-label" for="filter-${filter.type}">${formatStringToCapital(filter.type)}</label>
     </div>
   `);
